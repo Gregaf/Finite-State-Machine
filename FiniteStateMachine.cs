6d8a9dd84a62a385fde4542eb2d9bc3e34a5651f
Gregaf <39681927+Gregaf@users.noreply.github.com>
2018-05-27 22:28:11 -0400

    Add files via upload

---
+++ FiniteStateMachine.cs
@@ -0,0 +1,52 @@
+﻿
+public class FiniteStateMachine<T>
+{
+    private T Owner;
+    private FsmState<T> CurrentState;
+    private FsmState<T> PreviousState;
+
+    // Initialize
+    public void Awake()
+    {
+        CurrentState = null;
+        PreviousState = null;
+    }
+
+    // 'T' Class will be set to the owner (the class using the FSM).
+    // Then change the current state from null to the starting state.
+    public void Configure(T owner, FsmState<T> InitialState)
+    {
+        Owner = owner;
+        ChangeState(InitialState);
+    }
+
+    // Game loop
+    public void Update()
+    {
+        // If there is a state then Execute will be called allowing for the use of...
+        // Update in the State class.
+        if (CurrentState != null)
+            CurrentState.Execute(Owner);
+    }
+
+    public void ChangeState(FsmState<T> NewState)
+    {
+        // Change the previous state to the current state for more functionality.
+        PreviousState = CurrentState;
+
+        if (CurrentState != null)
+            CurrentState.Exit(Owner);
+
+        // Set the current state to new state
+        CurrentState = NewState;
+    
+        if (CurrentState != null)
+            CurrentState.Enter(Owner);
+    }
+
+    public void RevertToPreviousState()
+    {
+        if (PreviousState != null)
+            ChangeState(PreviousState);
+    }
+};
---
+++ FsmState.cs
@@ -0,0 +1,12 @@
+﻿
+abstract public class FsmState<T>
+{
+    // Called once when changing to this state.
+    abstract public void Enter(T owner);
+
+    // Continuously called when this = currentState (Bulk of logic would go here).
+    abstract public void Execute(T owner);
+
+    // Called once when changing from this state.
+    abstract public void Exit(T owner);
+}
